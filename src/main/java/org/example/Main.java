fc17a1ec632174a376df626bf9229bbe36985588
99kenny <helloiamkenny@gmail.com>
2022-12-02 12:40:27 +0900

    feat: dummy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,33 @@ package org.example;
 
 import org.example.post.Board;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    static Dummy dummy = new Dummy();
     static Board board = new Board();
+
     public static void main(String[] args) {
-//        board.createBoard("haha");
-//        board.readBoard("haha");
-//        board.updateBoard(2L, "huhu");
-        board.deleteBoard(2L);
+        System.out.println("\n" +
+                " _      _____ ______ ______   ___  ______ __   __\n" +
+                "| |    |_   _|| ___ \\| ___ \\ / _ \\ | ___ \\\\ \\ / /\n" +
+                "| |      | |  | |_/ /| |_/ // /_\\ \\| |_/ / \\ V / \n" +
+                "| |      | |  | ___ \\|    / |  _  ||    /   \\ /  \n" +
+                "| |____ _| |_ | |_/ /| |\\ \\ | | | || |\\ \\   | |  \n" +
+                "\\_____/ \\___/ \\____/ \\_| \\_|\\_| |_/\\_| \\_|  \\_/  \n" +
+                "                                                 \n" +
+                "                                                 \n");
+        Scanner choice = new Scanner(System.in);
+
+        while (true){
+            choice.nextInt();
+        }
+    }
+
+    public static void printMenu(){
+        System.out.println(1.);
+        System.out.println(2.);
+        System.out.println(3.);
+        System.out.println(4.);
     }
 }
