e5fd919147fc2fe279097439956eb9b4b9610026
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-12-02 15:52:27 +0900

    feat: implement enter, exit

--- src/main/java/org/example/user/VisitRecord.java
+++ src/main/java/org/example/user/VisitRecord.java
@@ -8,18 +8,79 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class VisitRecord {
+    private static final int ENTER = 0;
+    private static final int EXIT = 1;
     private final Connection conn = DBConnection.conn;
 
-    public void create(int type, Long userId) {
+    public void enter(String time, Long userId) {
         PreparedStatement ps = null;
         try {
-            String sql = String.format("INSERT INTO visit_record (type, user_id) VALUES (%d, %d)", type, userId);
+            String sql = String.format("INSERT INTO visit_record (time, type, user_id) " +
+                                        "VALUES ('%s', %d, %d)", time, ENTER, userId);
 
             ps = conn.prepareStatement(sql);
             ps.execute();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void exit(String time, Long userId) {
+        PreparedStatement ps = null;
+        try {
+            String sql = String.format("INSERT INTO visit_record (time, type, user_id) " +
+                                        "VALUES ('%s', %d, %d)", time, EXIT, userId);
+
+            ps = conn.prepareStatement(sql);
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void selectAll() {
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+        try {
+            String sql = "SELECT * FROM visit_record";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            System.out.println("------------------------------------------------------------------------------------------------------------------------------------------");
+            System.out.printf("%-20s %-20s %-20s %-20s", "VISIT RECORD ID", "TIME", "TYPE", "USERID");
+            System.out.println();
+            while (rs.next()){
+                System.out.format("%-20s %-20s %-20s %-20s",
+                        rs.getString("visit_record_id"), rs.getString("time"), rs.getString("type")
+                        ,rs.getString("user_id"));
+                System.out.println();
+            }
+            System.out.println("--------------------------------------------------------------------------------------------------------------------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
             if (ps != null) {
                 try {
                     ps.close();
