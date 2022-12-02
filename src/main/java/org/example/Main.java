8fa09aadfae3400e2e286a4f7a17f5775f67ff28
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-12-02 15:58:29 +0900

    feat: implement extend, visit cli

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,10 @@ import org.example.book.Book;
 import org.example.book.Borrow;
 import org.example.post.Board;
 import org.example.user.User;
+import org.example.user.VisitRecord;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +17,7 @@ public class Main {
     static User user = new User();
     static Book book = new Book();
     static Borrow borrow = new Borrow();
+    static VisitRecord visitRecord = new VisitRecord();
 
     public static void main(String[] args) {
         System.out.println("\n" +
@@ -31,41 +35,74 @@ public class Main {
         while (true){
             int c = choice.nextInt();
             switch (c){
-                //책 리스트
+                //도서관 들어오기
                 case 1:
+                    System.out.print("user id : ");
+                    Long borrowID = choice.nextLong();
+                    choice.nextLine();
+                    visitRecord.enter(getToday(), borrowID);
+                    break;
+                //도서관 나가기
+                case 2:
+                    System.out.print("user id : ");
+                    borrowID = choice.nextLong();
+                    choice.nextLine();
+                    visitRecord.exit(getToday(), borrowID);
+                    break;
+                //출입 기록
+                case 3:
+                    visitRecord.selectAll();
+                    break;
+                //책 리스트
+                case 4:
                     book.selectAll();
                     break;
 
                 //유저 데이터 출력
-                case 2:
+                case 5:
                     user.selectAll();
                     break;
-                case 3:
+                //책 빌리기
+                //빌리는중 BORROWING, 반납 RETURNED
+                case 6:
+                    Date borrowDay = new Date();
+                    Date returnDay = addDays(borrowDay, 7);
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    System.out.print("user id : ");
+                    Long userID = choice.nextLong();
+                    choice.nextLine();
+                    System.out.print("book id : ");
+                    String bookID = choice.nextLine();
+                    borrow.create("BORROWING", dateFormat.format(borrowDay), dateFormat.format(returnDay), userID, bookID);
+                    break;
+                //책 대여 기록 출력
+                case 7:
                     borrow.selectAll();
                     break;
+                //대여 연장
+                case 8:
+                    System.out.print("borrow id : ");
+                    borrowID = choice.nextLong();
+                    choice.nextLine();
+                    borrow.extendReturnDate(borrowID);
+                    break;
+                //대여 연장 기록
+                case 9:
+                    borrow.selectAllExtendList();
+                    break;
                 //유저 더미 데이터
-                case 4:
+                case 10:
                     System.out.print("num : ");
                     int num1 = choice.nextInt();
                     dummy.createUserDummy(num1);
                     System.out.println();
                     break;
                 //책 관련 더미 데이터
-                case 5:
+                case 11:
                     System.out.print("num : ");
                     int num2 = choice.nextInt();
                     dummy.createBookRelatedDummy(num2);
                     System.out.println();
-                    break;
-                //책 빌리기
-                //빌리는중 BORROWING, 반납 RETURNED
-                case 6:
-                    System.out.print("user id : ");
-                    Long userID = choice.nextLong();
-                    choice.nextLine();
-                    System.out.print("book id : ");
-                    String bookID = choice.nextLine();
-                    borrow.create("BORROWING", userID, bookID);
                     break;
             }
             printMenu();
@@ -74,12 +111,30 @@ public class Main {
     }
 
     public static void printMenu(){
-        System.out.println("1.book list");
-        System.out.println("2.user list");
-        System.out.println("3.borrow list");
-        System.out.println("4.add dummy users");
-        System.out.println("5.add dummy books");
+        System.out.println("1.enter library");
+        System.out.println("2.exit library");
+        System.out.println("3.visit list");
+        System.out.println("4.book list");
+        System.out.println("5.user list");
         System.out.println("6.borrow books");
+        System.out.println("7.borrow list");
+        System.out.println("8.extend return date");
+        System.out.println("9.extend list");
+        System.out.println("10.add dummy users");
+        System.out.println("11.add dummy books");
         System.out.print("choice : ");
+    }
+    public static String getToday()
+    {
+        Date today = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        return dateFormat.format(today);
+    }
+
+    public static Date addDays(Date d, int days)
+    {
+        d.setTime(d.getTime() + (long) days * 1000 * 60 * 60 * 24);
+        return d;
     }
 }
